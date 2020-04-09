6aed9d671914c0cf2c93a11415c6248f49d29321
Gustavo Santos <gustavo.1.santos-ext@jci.com>
2020-04-09 15:58:20 +0100

    base project

---
+++ kafka-consumer/src/main/java/com/practice/kafka/consumer/KafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.practice.kafka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerApplication.class, args);
+	}
+
+}
